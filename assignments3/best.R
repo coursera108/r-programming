da69331c3b5f3f8ff7a3100f3b3cc9f033353bfc
Coursera108 <coursera108@gmail.com>
2014-05-21 05:40:36 -0500

    Initial add of Programming Assignments 3

---
+++ assignments3/best.R
@@ -0,0 +1,20 @@
+best <- function(state, outcome) {
+    data.outcome <- read.csv("outcome-of-care-measures.csv", header=TRUE, colClasses="character")
+    outcomes <- c("heart failure", "heart attack", "pneumonia")
+    states <- unique(data.outcome$State)
+    if (!(state %in% states))
+        stop("invalid state")
+    if(!(outcome %in% outcomes))
+        stop("invalid outcome")
+    colnames(data.outcome)[colnames(data.outcome) == "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure"] <- "heart failure"
+    colnames(data.outcome)[colnames(data.outcome) == "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack"] <- "heart attack"
+    colnames(data.outcome)[colnames(data.outcome) == "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia"] <- "pneumonia"
+    data.outcome[, "heart failure"] <- as.numeric(data.outcome[, "heart failure"])
+    data.outcome[, "heart attack"] <- as.numeric(data.outcome[, "heart attack"])
+    data.outcome[, "pneumonia"] <- as.numeric(data.outcome[, "pneumonia"])
+    
+    data.outcome.state <- data.outcome[data.outcome$State == state,c("Hospital.Name", "heart failure", "heart attack", "pneumonia")]
+    outcome.sort <- data.outcome.state[order(with(data.outcome.state, get(outcome))), ]
+    outcome.sort[1,"Hospital.Name"]
+
+}
